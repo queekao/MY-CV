ce894f6e199ae5c565c8d1e8def0ee53240c33e5
queekao <87218631+queekao@users.noreply.github.com>
2021-08-30 19:27:22 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,48 @@ $(document).ready(function(){
  		autoplay:'true',
  		path:'https://assets3.lottiefiles.com/packages/lf20_3uoshewc.json'
 		})
+		var animation = bodymovin.loadAnimation({
+ 		container: document.getElementById('skill1'),
+ 		renderer:'svg',
+ 		loop:'',
+ 		autoplay:'true',
+ 		path:'https://assets2.lottiefiles.com/private_files/lf30_24p1bq00.json'
+		})
+		var animation = bodymovin.loadAnimation({
+ 		container: document.getElementById('skill2'),
+ 		renderer:'svg',
+ 		loop:'',
+ 		autoplay:'true',
+ 		path:'https://assets5.lottiefiles.com/private_files/lf30_pzt6fyzj.json'
+		})
+		var animation = bodymovin.loadAnimation({
+ 		container: document.getElementById('skill3'),
+ 		renderer:'svg',
+ 		loop:'',
+ 		autoplay:'true',
+ 		path:'https://assets1.lottiefiles.com/private_files/lf30_kmsqivyk.json'
+		})
+		var animation = bodymovin.loadAnimation({
+ 		container: document.getElementById('skill4'),
+ 		renderer:'svg',
+ 		loop:'',
+ 		autoplay:'true',
+ 		path:'https://assets2.lottiefiles.com/private_files/lf30_qhrpbr3t.json'
+		})
+		var animation = bodymovin.loadAnimation({
+ 		container: document.getElementById('skill5'),
+ 		renderer:'svg',
+ 		loop:'',
+ 		autoplay:'true',
+ 		path:'https://assets8.lottiefiles.com/private_files/lf30_80xbe56b.json'
+		})
+		var animation = bodymovin.loadAnimation({
+ 		container: document.getElementById('skill6'),
+ 		renderer:'svg',
+ 		loop:'',
+ 		autoplay:'true',
+ 		path:'https://assets9.lottiefiles.com/private_files/lf30_83oy3ycs.json'
+		})
 	// header color change
 	$(window).on("scroll",function(){
 		var scrollDistance = $(window).scrollTop();
