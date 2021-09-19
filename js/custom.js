4b6ec8b94faa74532871d9b58690c8cda454aa58
queekao <87218631+queekao@users.noreply.github.com>
2021-09-19 15:52:36 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,55 +1,62 @@
 $(document).ready(function(){
 	// lottie animation
-	$(window).ready(function(){
+$(window).ready(function(){
 		var animation = bodymovin.loadAnimation({
  		container: document.getElementById('logo'),
  		renderer:'svg',
- 		loop:'2',
+ 		loop:false,
  		autoplay:'true',
  		path:'https://assets5.lottiefiles.com/private_files/lf30_0g4jfhqn.json'
 		})
 		var animation = bodymovin.loadAnimation({
  		container: document.getElementById('skill1'),
  		renderer:'svg',
- 		loop:'',
+ 		loop:'3',
  		autoplay:'true',
  		path:'https://assets4.lottiefiles.com/private_files/lf30_coea7bte.json'
 		})
 		var animation = bodymovin.loadAnimation({
  		container: document.getElementById('skill2'),
  		renderer:'svg',
- 		loop:'',
+ 		loop:'3',
  		autoplay:'true',
  		path:'https://assets10.lottiefiles.com/packages/lf20_g4m1auuk.json'
 		})
 		var animation = bodymovin.loadAnimation({
  		container: document.getElementById('skill3'),
  		renderer:'svg',
- 		loop:'',
+ 		loop:'3',
  		autoplay:'true',
  		path:'https://assets10.lottiefiles.com/private_files/lf30_31yi2jtl.json'
 		})
 		var animation = bodymovin.loadAnimation({
  		container: document.getElementById('skill4'),
  		renderer:'svg',
- 		loop:'',
+ 		loop:'3',
  		autoplay:'true',
  		path:'https://assets7.lottiefiles.com/private_files/lf30_qrnxyorl.json'
 		})
 		var animation = bodymovin.loadAnimation({
  		container: document.getElementById('skill5'),
  		renderer:'svg',
- 		loop:'',
+ 		loop:'3',
  		autoplay:'true',
  		path:'https://assets3.lottiefiles.com/private_files/lf30_ztkzfr9e.json'
 		})
 		var animation = bodymovin.loadAnimation({
  		container: document.getElementById('skill6'),
  		renderer:'svg',
- 		loop:'',
+ 		loop:'3',
  		autoplay:'true',
  		path:'https://assets7.lottiefiles.com/private_files/lf30_zkxz853t.json'
 		})
+	$(document).ready(function(){
+		$('#logo').scroll(function(){
+			play.addEvenlistener('scroll',() => {
+				animItem.GoToAndPlay(0,true);
+			})
+		})
+	})	
 	// header color change
 	$(window).on("scroll",function(){
 		var scrollDistance = $(window).scrollTop();
@@ -60,7 +67,6 @@ $(document).ready(function(){
 		else{
 			$header.removeClass("header--colored");
 		}
-		
  	})
 		play.addEvenlistener('click', () => {
 			animItem.GoToAndPlay(0,true);
@@ -80,6 +86,8 @@ $(document).ready(function(){
 	if(screen.width > 768){
 		$(window).stellar();
 	}
+
 	
 
 })
+
